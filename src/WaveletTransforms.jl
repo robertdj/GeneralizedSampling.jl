80e7eaba218117fe239de3661fd65ae59a52e605
Robert DJ <git@dahl-jacobsen.dk>
2015-12-21 22:17:10 +0100

    Daubechies wavelet with dyadic dilation algorithm

--- src/WaveletTransforms.jl
+++ src/WaveletTransforms.jl
@@ -1,34 +1,159 @@
 # ------------------------------------------------------------
-# Scaling function
+# Scaling functions
 
+@doc """
+	HaarScaling(xi[, J, k])
+
+The Haar scaling function evaluated in `xi` at level `J` and translation `k`.
+By default, `J=0` and `k=0`.
+"""->
 function HaarScaling{T<:Real}(xi::T)
 	0 <= xi < 1 ? 1 : 0
 end
 
+@doc """
+	DaubScaling(N, L[, J, k])
+
+A Daubechies `N` scaling function at scale `J` and translation `k` evaluated in `0:2^(-L):2N-1`.
+"""->
+function DaubScaling(N::Int, L::Int)
+	wt = wavelet( WT.Daubechies{N}() )
+	phi, x = DaubScaling( wt.qmf, L )
+end
+
+function DaubScaling(C::Vector{Float64}, R::Int)
+	max_supp = support(C)*2^R
+	x = collect(0:max_supp) / 2^R
+	Nx = max_supp + 1
+	phi = zeros(Float64, Nx)
+
+	# Base level
+	cur_idx = level2idx(0, R, Nx)
+	phi[cur_idx] = DaubScaling(C)
+
+	# Recursion: Fill remaining levels
+	NC = length(C)
+	sqrt2 = sqrt(2)
+	for L = 1:R
+		cur_idx = level2idx(L, R, Nx)
+
+		for phin = cur_idx
+			for Ck = 1:NC
+				pidx = parent_scale_index(phin, Ck-1, R)
+				if 1 <= pidx <= Nx
+					phi[phin] += sqrt2*C[Ck]*phi[pidx]
+				end
+			end
+		end
+	end
+
+	return x, phi
+end
+
+@doc """
+	DaubScaling(C::Vector)
+
+Compute function values of the scaling function defined by the filter
+`C` at the integers in the support.
+"""->
+function DaubScaling(C::Vector{Float64})
+	NC = length(C)
+	filter_matrix = zeros(Float64, NC, NC)
+
+	sqrt2 = sqrt(2)
+	for nj = 1:NC, ni = 1:NC
+		Cidx = 2*ni - nj
+		if 1 <= Cidx <= NC
+			filter_matrix[ni, nj] = sqrt2*C[Cidx]
+		end
+	end
+
+	# Eigenvector of eigenvalue 1 (the largest)
+	# TODO: eigs is not consistent with sign
+	eigenvec = eigs(filter_matrix; nev=1)[2]
+	return real(vec(eigenvec))
+	#= return filter_matrix =#
+end
+
+@doc """
+	level2idx(L::Int, R::Int, N::Int) -> idx
+
+Return the indices at level `L` in a scaling function vector of length `N` at resolution `R`.
+"""->
+function level2idx(L::Int, R::Int, N::Int)
+	@assert 0 <= L <= R
+	@assert N >= 2^R
+
+	if L == 0
+		return collect( 1:2^R:N )
+	else
+		return 1 + collect( 2^(R-L):2^(R-L+1):N )
+	end
+end
+
+@doc """
+	support(C) -> length
+
+Return the length of the support of the scaling function defined by the
+filter vector `C`.
+"""->
+function support(C::Vector{Float64})
+	length(C) - 1
+end
+
+@doc """
+	parent_scale_index(i::Int, k::Int, L::Int)
+
+In the vector `x` where the `i`'th entry is `x_i = (i-1)/2^L`,
+`parent_scale_index` returns the index of `2x_i - k`.
+"""->
+function parent_scale_index(i::Int, k::Int, L::Int)
+	2*i - 1 - k*2^L
+end
+
+
 # ------------------------------------------------------------
-# Dilation, translation and vectorization
+# Dilation, translation
 
 function HaarScaling{T<:Real}(xi::T, J::Int)
-	2.0^(J/2)*HaarScaling(2.0^J*xi)
+	2.0^(J/2)*HaarScaling(2^J*xi)
 end
 
 function HaarScaling{T<:Real}(xi::T, J::Int, k::Int)
-	2.0^(J/2)*HaarScaling(2.0^J*xi - k)
+	# TODO: Is this error check slowing things down?
+	@assert 0 <= k < 2^J
+	2.0^(J/2)*HaarScaling(2^J*xi - k)
 end
 
+function DaubScaling(C::Vector{Float64}, L::Int, J::Int, k::Int=0)
+end
+
+# ------------------------------------------------------------
+# Vectorization
+
+@vectorize_1arg Real HaarScaling
+
 function HaarScaling{T<:Real}(xi::AbstractArray{T}, J::Int, k::Int)
-	map( xi -> HaarScaling(xi,J,k), xi )
+	N = length(xi)
+	y = Array(Float64, size(xi))
+	for n = 1:N
+		@inbounds y[n] = HaarScaling( xi[n], J, k )
+	end
+	return y
 end
 
 
 # ------------------------------------------------------------
 
 @doc """
-	weval(coef, J::Int, L::Int)
+	weval(coef, wave, L::Int)
 
-Evaluate `coef` vector in the Haar basis at scale `J` in the points
-`0:2^-L:1-2^-L`.
+Evaluate `coef` vector in the `wave` basis in the points `0:2^-L:1-2^-L`.
 """->
+function weval(coef::AbstractArray{Float64}, J::Int, wave::AbstractString, L::Int)
+end
+
+# Reconstruction in Haar basis
 function weval(coef::AbstractArray{Float64}, L::Int)
 	inc = 2.0^(-L)
 	x = collect( 0:inc:1-inc )
@@ -43,10 +168,16 @@ function weval(coef::AbstractArray{Float64}, L::Int)
 		for m = 1:Ncoef
 			# TODO: Only include the functions that have x[nx] in their
 			# support
-			y[n] += coef[m]*HaarScaling( x[n], J, m-1 )
+			@inbounds y[n] += coef[m]*HaarScaling( x[n], J, m-1 )
 		end
 	end
 
 	return x, y
+end
+
+# Reconstruction in general Daubechies basis
+function weval(coef::AbstractArray{Float64}, J::Int, N::Int, L::Int)
+	# TODO: J must be so large that the support of the wavelet is
+	# contained in [0,1]
 end
 
