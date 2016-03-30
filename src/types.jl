3023fc36078d3d378057666ab870149a37d41e7f
Robert DJ <git@dahl-jacobsen.dk>
2016-03-30 19:41:41 +0200

    Different CoB types with and without boundary

--- src/types.jl
+++ src/types.jl
@@ -1,5 +1,5 @@
 @doc """
-`CoB` is an abstract *c*hange *o*f *b*asis super-type 
+`CoB` is an abstract **c**hange **o**f **b**asis super-type 
 """->
 abstract CoB
 
@@ -8,22 +8,47 @@ abstract CoB
 # Frequency to wavelets
 
 @doc """
-`Freq2wave` is a change of basis type between frequency samples and wavelets. 
+`Freq2Wave` is a change of basis type between frequency samples and wavelets. 
+
+There are sub types for wavelets with and without boundary correction.
 """->
-immutable Freq2wave{D} <: CoB
-	# Sampling
-	samples::Array{Float64,D}
-	weights::Nullable{Vector{Float64}}
+abstract Freq2Wave <: CoB
 
-	# Reconstruction
-	wave::AbstractString
-	column1::Vector{Complex{Float64}}
-	# TODO: J is redundant; remove?
-	J::Int
+macro common_freq2wave()
+	esc(quote
+		# Sampling
+		# TODO: Are the parts of samples saved in NFFT sufficient for collect?
+		samples::Array{Float64, D}
+		weights::Nullable{Vector{Float64}}
 
-	# Multiplication with FFT: T*x = diag*NFFT(x)
-	diag::Vector{Complex{Float64}}
-	FFT::NFFT.NFFTPlan{D,Float64}
+		# Reconstruction
+		J::Int
+		wavename::AbstractString
+
+		# In 1D: T = [left diag*NFFT right]
+		diag::Vector{Complex{Float64}}
+		NFFT::NFFT.NFFTPlan{D,Float64}
+	end)
+end
+
+# Uniform samples, no boundary correction
+immutable Freq2NoBoundaryWave{D} <: Freq2Wave
+	@common_freq2wave()
+end
+
+# Uniform samples, boundary correction
+immutable Freq2BoundaryWave{D} <: Freq2Wave
+	@common_freq2wave()
+	left::Matrix{Complex{Float64}}
+	right::Matrix{Complex{Float64}}
+end
+
+function isuniform(T::Freq2Wave)
+	!isnull(T.weights)
+end
+
+function withboundary(T::Freq2Wave)
+	isdefined(T, :left)
 end
 
 
