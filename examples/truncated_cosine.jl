91e9cae2e06723951c2d7160fb266173352f61ef
Robert DJ <git@dahl-jacobsen.dk>
2016-07-09 12:47:08 +0200

    Updating examples

--- examples/truncated_cosine.jl
+++ examples/truncated_cosine.jl
@@ -18,34 +18,6 @@ function ftcos(xi)
 	end
 end
 
-#=
-# [-1,1]
-function tcos(x)
-	if 0.0 <= x <= 1.0
-		return cos(pi*x)
-	else
-		return 0.0
-	end
-end
-
-# [-1,1] wrt exp(-2pi*im*xi*x)
-#= function ftcos(xi) =#
-#= 	if abs(xi) == 0.5 =#
-#= 		return 0.5 + 0.0*im =#
-#= 	else =#
-#= 		return 2*im*xi*( 1 + exp(-2*im*pi*xi) ) / (pi*(1 - 4*xi^2)) =#
-#= 	end =#
-#= end =#
-
-# [-1,1] wrt exp(-pi*im*xi*x)
-function ftcos(xi)
-	if abs(xi) == 1.0
-		return 0.5 + 0.0*im
-	else
-		return im*xi*( 1 + exp(-im*pi*xi) ) / (pi*(1 - xi^2))
-	end
-end
-=#
 
 # Overload tcos and ftcos to vector input
 @vectorize_1arg Float64 tcos
@@ -64,11 +36,7 @@ xi = GeneralizedSampling.grid(M, 0.5)
 f = ftcos(xi)
 
 T = Freq2Wave(xi, "haar", J)
-#= T = Freq2Wave(xi, "db2", J) =#
 wcoef = T \ f
-#= wcoef, h = lsqr(T, f) =#
-A = collect(T)
-#= wcoef = A \ f =#
 
 
 # ------------------------------------------------------------
@@ -78,7 +46,6 @@ using IntervalWavelets
 using Winston
 
 x, yw = weval( real(wcoef), "haar", 10 )
-#= x, yw = weval( real(wcoef), "db2", 10 ) =#
 
 plot(x, yw, x, tcos(x))
 
