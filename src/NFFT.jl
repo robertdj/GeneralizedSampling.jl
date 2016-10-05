a5871e1d58e15e0b4e8f7ed5602baf4507a200cd
Robert DJ <git@dahl-jacobsen.dk>
2016-10-05 09:49:28 +0200

    FFT for 2D adjoint multiplication

--- src/NFFT.jl
+++ src/NFFT.jl
@@ -2,7 +2,7 @@
 @inline myfft_adjoint!(A::StridedArray, P::NFFT.NFFTPlan, B::AbstractArray) = NFFT.nfft_adjoint!(P, B, A)
 
 
-@generated function myfft!{D}(A::StridedVector{Complex{Float64}}, P::FFTPlan, B::AbstractArray{Complex{Float64}, D})
+@generated function myfft!{D}(A::StridedVector{Complex{Float64}}, P::FFTPlan{D}, B::AbstractArray{Complex{Float64}, D})
     quote
         length(A) == prod(P.M) || throw(DimensionMismatch())
         size(B) == P.N || throw(DimensionMismatch())
@@ -42,6 +42,34 @@ function myfft_adjoint!(A::StridedVector{Complex{Float64}}, P::FFTPlan, B::Abstr
     for l in 1:P.N[1]
         A[l] = P.FFTvec[idx] * conj(P.pre_phaseshift[l])
         idx += P.q[1]
+    end
+
+    return A
+end
+
+function myfft_adjoint!(A::StridedMatrix{Complex{Float64}}, P::FFTPlan, B::AbstractVector{Complex{Float64}})
+    size(A) == P.N || throw(DimensionMismatch())
+    length(B) == prod(P.M) || throw(DimensionMismatch())
+
+    fill!(P.FFTvec, ComplexZero)
+    idx = 0
+    for l2 in 1:P.M[2]
+        for l1 in 1:P.M[1]
+            idx += 1
+            P.FFTvec[l1, l2] = B[idx] * conj(P.post_phaseshift[idx])
+        end
+    end
+
+    P.backwardFFT * P.FFTvec
+
+    idx2 = 1
+    for l2 in 1:P.N[2]
+        idx1 = 1
+        for l1 in 1:P.N[1]
+            A[l1, l2] = P.FFTvec[idx1, idx2] * conj(P.pre_phaseshift[l1, l2])
+            idx1 += P.q[1]
+        end
+        idx2 += P.q[2]
     end
 
     return A
